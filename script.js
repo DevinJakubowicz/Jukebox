ba1b64845f4bd65d7b79fe830ce35073b640895a
Devin Jakubowicz <devinjakub@gmail.com>
2016-11-29 17:26:31 -0500

    re-added functions for the play, pause and stop buttons

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
 
-// function Jukebox () {
-// 	this.songs [];
-// 	this.addTrack = function(song) {
-// 		this.song.push(song);
-// 		}	
-// }
 
 var audio;
 var dir = ""
 var playlist ["","",""];
 var playlist_index = 0;
 var ext = ".mp3";
-
+var playButton = playButton.addEventListener("click", playPause)
+var stopButton = stopButton.addEventListener("click", stop)
 audio = new Audio();
 audio.src = dir +playlist[0]+ext;
 audio.loop = false;
-
+audio.play_audio();
 
 function Jukebox () {
 
@@ -28,38 +23,44 @@ function Jukebox () {
 			this.songlibrary.push(newTrack);
 		}
 
+		function switchSong(){
+			if (playlist_index == (playlist.length - 1)){
+				playlist_index = 0:
+			}
+				else {
+					playlist_index++;
+				}
+				audio.src = dir+playlist[playlist_index]+ext;
+				audio.play();
+		}
+
+
+
+		function playPause() {
+			if (audio.paused) {
+				audio.play();
+				playButton.style.background = url("https://cdn3.iconfinder.com/data/icons/player/128/sound-08-128.png") no-repeat;
+			}
+				else {
+					audio.pause()
+					playButton.style.background = url("https://cdn0.iconfinder.com/data/icons/most-useful-icons/50/PAUSE-128.png") no-repeat;
+				}
+		}
+
+		function stop () {
+			if (audio.stopped) {
+				audio.stopped = false;
+				stopButton.style.background = url("http://www.myiconfinder.com/uploads/iconsets/128-128-f53b4ab743ce25cc599e7384e68c8f07.png") no-repeat;
+			}
+				else {
+					audio.stopped = true;
+					stopButton.style.background = url("http://www.myiconfinder.com/uploads/iconsets/128-128-f53b4ab743ce25cc599e7384e68c8f07.png")
+				}
+		}
+
+
+
 	};
 
 
-// document.addEventListener("DOMContentLoaded", function() { startplayer(); }, true);
-// var Jukebox;
-
-// function startplayer() 
-// {
-//  Jukebox = document.getElementById('player');
-//  // player.controls = false;
-// }
-
-
-// function play_audio() 
-// {
-//  Jukebox.play();
-// } 
-
-// function pause_audio() 
-// {
-//  Jukebox.pause();
-// }
-
-
-// function stop_audio() 
-// {
-//  Jukebox.pause();
-//  Jukebox.currentTime = 0;
-// }
-
-// function change_vol()
-// {
-//  Jukebox.volume=document.getElementById("change_vol").value;
-// }
 
