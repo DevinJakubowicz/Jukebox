4f9e791068f92966eba7c245dc11ec7cfa08112b
Devin Jakubowicz <devinjakub@gmail.com>
2016-11-29 21:19:44 -0500

    added titles of the songs

--- script.js
+++ script.js
@@ -3,15 +3,19 @@ function Jukebox () {
 
 
 	var audio, playButton, pauseButton, stopButton, nextSong, play, pause, switchSong;
+	var songName;
 	var dir = "../Jukebox/";
 	var playlist = ["kidRock","Hozier","deathCab"];
 	var playlist_index = 0;
 	var ext = ".mp3";
 
+	songs = document.getElementById("songName")
 	playButton = document.getElementById("playBtn");
 	pauseButton = document.getElementById("pauseBtn");
 	stopButton = document.getElementById("stopBtn");
 	nextSong = document.getElementById("nextBtn");
+
+
 
 	playButton = playButton.addEventListener("click", play);
 	pauseButton = pauseButton.addEventListener("click", pause);
@@ -21,6 +25,7 @@ function Jukebox () {
 	audio.src = dir+playlist[0]+ext;
 	audio.loop = false;
 	audio.play();
+	songs.innterHTML = "Track" + (playlist_index+1)+" - "+playlist[playlist_index]+ext;
 
 		songlibrary = [];
 
@@ -38,6 +43,7 @@ function Jukebox () {
 				else {
 					playlist_index++;
 				}
+				songs.innerHTML = "Track "+(playlist_index+1)+" - "+ playlist[playlist_index]+ext;
 				audio.src = dir+playlist[playlist_index]+ext;
 				audio.play();
 		}
